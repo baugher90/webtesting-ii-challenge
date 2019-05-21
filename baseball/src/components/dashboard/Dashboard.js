436a7eba33074981643f7180450bcb057d5ffecd
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-21 17:04:07 -0400

    render the components test passed

---
+++ baseball/src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+// provide a button that the person in charge can press every time there is a strike, ball, foul or hit.
+// there is NO need to specify the type of hit (single, double, etc).
+// changes recorded on this component should update the information shown by the Display component.
+
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ baseball/src/components/display/Display.js
@@ -0,0 +1,13 @@
+// display the count of balls and strikes for the at-bat.
+// should be updated when the user records activity on the Dashboard component.
+import React from 'react';
+
+function Display() {
+  return (
+    <div className="Display">
+      <h2>Display</h2>
+    </div>
+  );
+}
+
+export default Display;
